90e3ae86d9856a8f9690b36fccc033665eaf2b2d
mohdnihadkp1 <mohdnihadkp@gmail.com>
2026-04-26 15:00:25 +0530

    Delete find_map.mjs

--- find_map.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const content = fs.readFileSync('src/components/MapSection.tsx', 'utf8');
-const regex = /(?<!https:)(?<!http:)\/\/[^\n].{0,150}/g;
-const matches = content.match(regex);
-console.log(matches);
